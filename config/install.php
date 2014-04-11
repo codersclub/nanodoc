d1e569a269dfcf046256198ebbfd16ecf1d98ca3
raqqun <auto.anti@gmail.com>
2014-04-11 23:28:13 +0200

    delete header.php/footer.php

--- config/install.php
+++ config/install.php
@@ -1,7 +1,25 @@
 <?php require_once 'setup_functions.php'; ?>
 
-<?php require_once 'header.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php echo 'NanoDoc Installation'; ?></title>
 
+        <!-- Bootstrap -->
+        <link href="../admin/css/bootstrap.min.css" rel="stylesheet">
+        <!-- NannoDoc custom css -->
+        <link rel="stylesheet" href="../admin/css/custom-style.css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
     <div class="jumbotron config">
         <h1>Welcome to NanoDoc</h1>
 
@@ -61,4 +79,9 @@
 
     </div>
 
-<?php require_once 'footer.php'; ?>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="../admin/js/bootstrap.min.js"></script>
+    </body>
+</html>
