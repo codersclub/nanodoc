677ae85d0ecca91fdb678a487392615232181631
raqqun <auto.anti@gmail.com>
2014-04-15 21:22:39 +0200

    fix getOption call

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ $nd_mysql->checkDatabase(); ?>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?php echo $nd_mysql->getOptions('nd_title') . ' &rsaquo; login'; ?></title>
+        <title><?php echo $nd_mysql->getOption('nd_title') . ' &rsaquo; login'; ?></title>
 
         <!-- Bootstrap -->
         <link href="admin/css/bootstrap.min.css" rel="stylesheet">
