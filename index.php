14c73ac805a99ad33f2f1f10e7628df20c138c68
raqqun <auto.anti@gmail.com>
2014-04-16 20:37:01 +0200

    check if database file exists or go to install

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
 
-
-require_once 'themes/default/index.php';
+if (file_exists('nanodoc.sq3')) {
+	require_once 'themes/default/index.php';
+} else {
+	header('Location: config/install.php');
+}
+?>
