9fa430b786a56d9b029062fdd36e14c126c30300
raqqun <auto.anti@gmail.com>
2014-04-16 20:39:09 +0200

    change conditions and info display

--- config/install.php
+++ config/install.php
@@ -20,64 +20,61 @@
         <![endif]-->
     </head>
     <body>
-    <div class="jumbotron config">
-        <h1>Welcome to NanoDoc</h1>
+        <div class="jumbotron config">
+            <h1>Welcome to NanoDoc</h1>
+            
+    <?php if (file_exists('../nanodoc.sq3')) { ?>
         
-<?php if (file_exists('../nanodoc.sq3')) { ?>
-    
-    <?php if(empty($_GET['step'])) { ?>
-        <p>Nanodoc is already installed. You can login now</p>
-        <a class="btn btn-primary btn-lg" href="../login.php">Login</a>
-    <?php } else { ?>
+            <p>Nanodoc is already installed. You can login now.</p>
+            <a class="btn btn-primary btn-lg" href="../login.php">Login</a>
 
-    <?php 
-        switch ($_GET['step']) {
+        <?php } elseif (!empty($_GET['step'])) { ?>
 
-            case '1': ?>
-                <form id="install-form" action="install.php?step=2" method="post">
-                    <table class="table">
-                        <tr>
-                            <th>Site Title <span>*</span></th>
-                            <td><input type="text" class="form-control" name="nd_title"></td>
-                            <td>Choose a Title for your Nanodoc.</td>
-                        </tr>
-                        <tr>
-                            <th>Username <span>*</span></th>
-                            <td><input type="text" class="form-control" name="nd_user"></td>
-                            <td>Choose a username for your Nanodoc.</td>
-                        </tr>
-                        <tr>
-                            <th>Password <span>*</span></th>
-                            <td><input type="text" class="form-control" name="nd_pass"></td>
-                            <td>Choose a password for your Nanodoc.</td>
-                        </tr>
-                        <tr>
-                            <th>Email <span>*</span></th>
-                            <td><input type="text" class="form-control" name="nd_user_email"></td>
-                            <td>Your email address.</td>
-                        </tr>
-                    </table>
-                    <span>* required fields</span>
-                    <button class="btn btn-primary btn-lg" type="submit">Install Nanodoc</button>
-                </form>
-                <?php break; 
+            <?php 
+                switch ($_GET['step']) {
 
-            case '2':
-                install_nanodoc();
-                break;
-        } // end switch?>
+                    case '1': ?>
+                        <form id="install-form" action="install.php?step=2" method="post">
+                            <table class="table">
+                                <tr>
+                                    <th>Site Title <span>*</span></th>
+                                    <td><input type="text" class="form-control" name="nd_title" required></td>
+                                    <td>Choose a Title for your Nanodoc.</td>
+                                </tr>
+                                <tr>
+                                    <th>Username <span>*</span></th>
+                                    <td><input type="text" class="form-control" name="nd_user" required></td>
+                                    <td>Choose a username for your Nanodoc.</td>
+                                </tr>
+                                <tr>
+                                    <th>Password <span>*</span></th>
+                                    <td><input type="text" class="form-control" name="nd_pass" required></td>
+                                    <td>Choose a password for your Nanodoc.</td>
+                                </tr>
+                                <tr>
+                                    <th>Email <span>*</span></th>
+                                    <td><input type="text" class="form-control" name="nd_user_email" required></td>
+                                    <td>Your email address.</td>
+                                </tr>
+                            </table>
+                            <span>* required fields</span>
+                            <button class="btn btn-primary btn-lg" type="submit">Install Nanodoc</button>
+                        </form>
+                        <?php break; 
 
-    <?php } // end if empty($_GET['step']) ?>
+                    case '2':
+                        install_nanodoc();
+                        break;
+                } // end switch?>
 
-<?php } else { // end if file_existed ?>
+    <?php } else { // end elseif empty ?>
 
-            <p>There is not a configuration file for your NanoDoc. You must create one. Please click on Create Config</p>
-            <a class="btn btn-primary btn-lg" href="setup.php?step=1">Create Config</a>
-<?php } ?>
-    
+            <p>You must create a database file first. Please click on Create Database</p>
+            <a class="btn btn-primary btn-lg" href="install.php?step=1">Create Database</a>
+
+    <?php } ?>
+
         </div> <!-- close welcome div -->
-
-    </div>
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
